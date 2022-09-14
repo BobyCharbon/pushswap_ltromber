faa1b16a163dd7cced5963e41b3eda3a49235dc7
Boby <boby01170@gmail.com>
2022-09-14 23:34:09 +0200

    update tests main

--- main.c
+++ main.c
@@ -8,33 +8,20 @@ int	main(int argc, char **argv)
 	t_node	*temp;
 	int	i = 0;
 	int	retour = 0;
-	if (argc < 2)
+	/* if (argc < 2)
 	{
 		printf("Pas assez d'argument");
 		return(0);
-	}
+	} */
 
 
 	/* (void) argc;
 	(void) argv; */
 	list_a = init();
 	list_b = init();
-	parsing(list_a, argc, argv);
-	/* insert_top(list_a, 10);
-	insert_top(list_a, 63);
-	insert_top(list_a, 79); */
-	/* insert_data(list_a, 4);
-	insert_data(list_a, 6);
-	insert_data(list_a, 56);
-	insert_data(list_a, 7);
-	insert_data(list_a, 4);
-	insert_data(list_a, 27);
-	insert_data(list_a, 2);
-	insert_data(list_a, 12);
-	insert_data(list_a, 15 );*/
+
 	
-	/* insert_data(list_a, 12, 0);
-	insert_data(list_a, 2, 0);
+	/* insert_data(list_a, 2, 0);
 	insert_data(list_a, 6, 0);
 	insert_data(list_a, -8, 0);
 	insert_data(list_a, 40, 0);
@@ -43,15 +30,28 @@ int	main(int argc, char **argv)
 	insert_data(list_a, 95, 0);
 	insert_data(list_a, 98, 0);
 	insert_data(list_a, 92, 0);
-	insert_data(list_a, 94, 0);
-	insert_data(list_a, 96, 0); */
+	insert_data(list_a, 94, 0); */
+	
+	parsing(list_a, argc, argv);
 	display_pos(list_a);
-	// rev_rotate_a(list_a);
-	// display_list(list_a);
 	set_pos(list_a);
-	// rotate_a(list_a);
 	tri_selectif(list_a, list_b);
 	display_pos(list_a);
+
+	/* printf("%d\n",is_correct("hello"));
+	printf("%d\n",is_correct("123"));
+	printf("%d\n",is_correct("12hello"));
+	printf("%d\n",is_correct("")); */
+
+	/* insert_top(list_a, 10);
+	insert_top(list_a, 63);
+	insert_top(list_a, 79); */
+	
+	/* insert_data(list_a, 12, 0);
+	insert_data(list_a, 96, 0); */
+	// rev_rotate_a(list_a);
+	// display_list(list_a);
+	// rotate_a(list_a);
 	// set_pos(list_a);
 	
 
